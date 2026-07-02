7913e2f7a1b2cdbb851a970bbb5ea0bf95c5982e
muxriddin-web <otkirovmuxriddin01@gmail.com>
2026-07-02 12:50:20 +0500

    Comment out MongoDB connection code in db.js
    Commented out the existing MongoDB connection code in db.js.

--- project1 web/backend/config/db.js
+++ project1 web/backend/config/db.js
@@ -1,19 +1,19 @@
 //  3. config/db.js — MongoDB ulanish
 // config/db.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 86400000   // ⏳ 10 soniyagacha kutadi
-    });
-    console.log(`✅ MongoDB ulandi: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB ulanishda xatolik:", error.message);
-    process.exit(1); // jarayonni to‘xtatadi
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 86400000   // ⏳ 10 soniyagacha kutadi
+//     });
+//     console.log(`✅ MongoDB ulandi: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error("❌ MongoDB ulanishda xatolik:", error.message);
+//     process.exit(1); // jarayonni to‘xtatadi
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
