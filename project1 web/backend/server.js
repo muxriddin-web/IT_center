fc3335b3645a1ec0a18ea33f2c9d0cd114b3c48f
Muxriddin O'tkirov <otkirovmuxriddin01@gmail.com>
2026-08-24 23:13:32 +0500

    Update server.js

--- project1 web/backend/server.js
+++ project1 web/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const cors = require('cors');
-// const 
+
 
 // ENV
 dotenv.config(); // Atrof-muhit o'zgaruvchilari faollashtirildi
